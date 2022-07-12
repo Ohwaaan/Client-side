02029f31096825f4fb42c34925ef886864f90a9f
Ohwaaan <owenlian88@gmail.com>
2022-07-12 19:39:05 -0400

    minor edits

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -10,11 +10,13 @@ const StudentView = (props) => {
   // Render a single Student view 
   return (
     <div>
+      <img src = {"https://sportlinx360.com/sites/default/files/public_uploads/Website_content/Website_Logos/graduate-student-avatar.png"} alt ="default student"/>
       <h1>{student.firstname + " " + student.lastname}</h1>
       <h3>{student.campus.name}</h3>
+      <h3>{student.email}</h3>
+      <h3>{student.gpa}</h3>
     </div>
   );
-
 };
 
 export default StudentView;
